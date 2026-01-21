a207d48961b9eaa32b3b5690a472d98d0890e78a
Aborav <anton34579@gmail.com>
2026-01-21 17:42:40 +0300

    just nothing

--- M_Filter.h
+++ M_Filter.h
@@ -5,27 +5,23 @@
 
 
 //Signal Filter mini lib
-////////////////////////////////////////////////////////////////
+
+/*
+Ho to use:
+-create filtering buffer
+
+*/
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-#define max(a,b) \
-   ({ __typeof__ (a) _a = (a); \
-       __typeof__ (b) _b = (b); \
-     _a > _b ? _a : _b; })
 
-#define min(a,b) \
-   ({ __typeof__ (a) _a = (a); \
-       __typeof__ (b) _b = (b); \
-     _a < _b ? _a : _b; })
          
-/* template <typename X> float MedianFilter(X meas) {
-    static float buf[3];
-    static uint8_t count = 0;
-    buf[count] = meas;
-    if (++count >= 3) count = 0;
-    return (max(buf[0], buf[1]) == max(buf[1], buf[2])) ? max(buf[0], buf[2]) : max(buf[1], min(buf[0], buf[2]));
-} */
 
+/*
+ * @brief median filter function  
+ * @param[out] *meas -> pointer to a filtering variable (uint16_t)
+ * @param[in] buf -> mediana filter buffer
+ */
 void MedianFilter(uint16_t *meas, uint16_t buf[]) {
   buf[0] = buf[1];
   buf[1] = buf[2];
@@ -33,6 +29,11 @@ void MedianFilter(uint16_t *meas, uint16_t buf[]) {
   *meas = (max(buf[0], buf[1]) == max(buf[1], buf[2])) ? max(buf[0], buf[2]) : max(buf[1], min(buf[0], buf[2]));
 }
 
+/*
+ * @brief running average filter function  
+ * @param[in] *new_val -> pointer to a filtering variable
+ * @param[in] k -> averaging factor
+ */
 uint16_t ExpRunAverage(uint16_t *new_val, float k) {
   static float filt_val = 0;
   filt_val += (*new_val - filt_val) * k;
@@ -41,3 +42,12 @@ uint16_t ExpRunAverage(uint16_t *new_val, float k) {
 
 
 #endif
+
+// C++
+/* template <typename X> float MedianFilter(X meas) {
+    static float buf[3];
+    static uint8_t count = 0;
+    buf[count] = meas;
+    if (++count >= 3) count = 0;
+    return (max(buf[0], buf[1]) == max(buf[1], buf[2])) ? max(buf[0], buf[2]) : max(buf[1], min(buf[0], buf[2]));
+} */
